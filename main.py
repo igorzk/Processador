dd559c116bb7d18ca05eaf2cb724d5aa83a85ba4
t01001 <t01001@t01001>
2022-03-22 19:58:18 -0300

    cors4

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"]
